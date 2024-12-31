975f5ce9eeb1589624fd8f6e430feba067460175
Abdullrahman011 <abo3jlan4455@gmail.com>
2025-01-01 00:40:33 +0300

    refactor: update delete product endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 const nextConfig = {
   output: 'standalone',
-  images: { unoptimized: true }
+  images: { 
+    unoptimized: true,
+    domains: ['alyoum-special.netlify.app', 'res.cloudinary.com']
+  }
 } satisfies import('next').NextConfig
 
 export default nextConfig;
