32df037417756312051d00f77b83f1aac7b12595
Abdullrahman011 <abo3jlan4455@gmail.com>
2025-01-01 14:21:09 +0300

    refactor: update delete product endpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,35 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ['"Marhey"', "sans-serif"], // تعيين الخط الجديد كـ sans الافتراضي
-      },
-    },
-  },
-  plugins: [],
+ content: [
+   "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+   "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+ ],
+ theme: {
+   extend: {
+     colors: {
+       background: "var(--background)",
+       foreground: "var(--foreground)",
+     },
+     fontFamily: {
+       sans: ['"Marhey"', "sans-serif"],
+     },
+     display: ['no-scrollbar'],
+   },
+ },
+ plugins: [
+   plugin(({ addUtilities }) => {
+     addUtilities({
+       '.no-scrollbar::-webkit-scrollbar': {
+         'display': 'none',
+       },
+       '.no-scrollbar': {
+         '-ms-overflow-style': 'none',
+         'scrollbar-width': 'none',
+       },
+     })
+   }),
+ ],
 } satisfies Config;
