8536e19ee7073a7c6fb71debb07c96c2398f1a22
Abdullrahman011 <abo3jlan4455@gmail.com>
2025-01-03 10:46:52 +0300

    refactor: update delete product endpoint

--- src/components/GoogleTags.tsx
+++ src/components/GoogleTags.tsx
@@ -6,7 +6,7 @@ export default function GoogleTags() {
         <>
             {/* Google Analytics */}
             <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=G-V29BMZFJPC`}
+                src="https://www.googletagmanager.com/gtag/js?id=G-V29BMZFJPC"
                 strategy="afterInteractive"
             />
             <Script id="google-analytics" strategy="afterInteractive">
@@ -15,28 +15,6 @@ export default function GoogleTags() {
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-V29BMZFJPC');
-                `}
-            </Script>
-
-            {/* Google AdSense */}
-            <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                strategy="afterInteractive"
-                data-ad-client="ca-pub-6285603860927889"
-            />
-            <Script id="google-adsense" strategy="afterInteractive">
-                {`
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                `}
-            </Script>
-
-            {/* Google Ads */}
-            <Script id="google-ads" strategy="afterInteractive">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('config', 'G-BPXPVW8P5E');
                 `}
             </Script>
         </>
