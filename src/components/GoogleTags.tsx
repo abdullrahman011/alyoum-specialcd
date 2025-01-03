feaa9ff6c83c90c8b95864b5271d5441a96a0ec9
Abdullrahman011 <abo3jlan4455@gmail.com>
2025-01-03 10:37:12 +0300

    refactor: update delete product endpoint

--- src/components/GoogleTags.tsx
+++ src/components/GoogleTags.tsx
@@ -7,33 +7,22 @@ export default function GoogleTags() {
             {/* Google Analytics */}
             <Script
                 src={`https://www.googletagmanager.com/gtag/js?id=G-V29BMZFJPC`}
-                strategy="afterInteractive"
+                strategy="lazyOnload"
             />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="google-analytics">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', 'G-V29BMZFJPC', {
-                        cookie_flags: 'SameSite=None;Secure'
-                    });
+                    gtag('config', 'G-V29BMZFJPC');
                 `}
             </Script>
 
-            {/* Google AdSense */}
-            <Script 
-                id="adsbygoogle"
-                async 
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                data-ad-client="ca-pub-6285603860927889"
-                strategy="lazyOnload"
+            {/* Google AdSense - تم تبسيط الكود */}
+            <Script
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6285603860927889"
+                async
                 crossOrigin="anonymous"
-                onLoad={() => {
-                    console.log('AdSense loaded successfully');
-                }}
-                onError={(e) => {
-                    console.error('AdSense loading error:', e);
-                }}
             />
         </>
     );
